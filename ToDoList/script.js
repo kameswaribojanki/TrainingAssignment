89bc6b6c02ed1d8ff5509b98e7a7a37607d33c1b
kameswaribojanki <kameswari.bojanki@gmail.com>
2022-07-04 16:03:58 +0530

    change

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -16,35 +16,31 @@ $(".btn").on("click", function (event) {
 });
 function addItemsToList(value) {
     items.push(value);
-    //$(".item-list").empty();
     readArrData(items);
-    //$(".input-item").val("");
 }
 
-$(".item-list").on("click",function (event) {
+$(".item-list").on("click",".li-item",function (event) {
     event.preventDefault();
-    var value = $(event.target).text();
+    var value = $(this).text();
     indexid = items.indexOf(value);
     $(".input-item").val(value);
     $(".btn").text("update");
 });
 $(document).on("click", ".remove", function () {
-        var currentIndex = items.indexOf($(this).parent(".li-item").text());
-        $(this).parent(".li-item").remove();
+        var currentIndex = items.indexOf($(this).parent("li").text());
+        $(this).parent("li").remove();
         items.splice(currentIndex, 1);
       });
 function updateItemsInList() {
     var value = $(".input-item").val();
     items[indexid] = value;
-    //$(".item-list").empty();
     readArrData(items);
     $(".btn").text("Store");
-    //$(".input-item").val("");
 }
 function readArrData(items) {
     $(".item-list").empty();
     for (let i = 0; i < items.length; i++) {
-    $(".item-list").append(`<li class="li-item"><span>${items[i]}</span> <span class="remove"><i class="fa-solid fa-trash"></i></span></li>`);
+    $(".item-list").append(`<li class=""><span class="li-item">${items[i]}</span> <span class="remove"><i class="fa-solid fa-trash"></i></span></li>`);
     }
     $(".input-item").val("");
 }
