89f546fad8a9ede3382e5dbf8b33773d9e07bfec
kameswaribojanki <kameswari.bojanki@gmail.com>
2022-07-06 18:21:15 +0530

    change

--- Assignment-5/script.js
+++ Assignment-5/script.js
@@ -5,3 +5,46 @@ $("button").click(function () {
   $("button").click(function () {
     $("buttton").toggleClass("custom-btn");
   });
+customQuotes();
+function customQuotes(){
+  $.getJSON("favorites.json",(data)=>{
+    console.log(data.vehicles);
+    for(let i=0;i<data.vehicles.length;i++){
+      $(".card").append(`
+      <div class="card-header d-flex align-items-center justify-content-between p-3 bg-primary text-white">
+      <h4>Custom Lease</h4>
+      <i class="fa-solid fa-clipboard-check fs-3"></i>
+  </div>
+  <div class="card-body bg-light">
+      <h3>2022 ${data.vehicles[i].year} ${data.vehicles[i].transmission} ${data.vehicles[i].trim}</h3>
+      <p>CVT AWD</p>
+      <img src="${data.vehicles[i].photoURLs}" class="mx-auto d-block">
+  </div>
+  <div class="card-footer bg-white pt-4 pb-4 ps-5 pe-5">
+      <div class="d-flex justify-content-between">
+          <p>Lunar Silver Metallic</p>
+          <p class="dot1"></p>
+      </div>
+      <div class="d-flex justify-content-between">
+          <p>Black Leather Interior</p>
+          <p class="dot2"></p>
+      </div>
+      <div class="d-flex justify-content-between">
+          <p>Your Price</p>
+          <p class="text-dark">$32,525</p>
+      </div>
+      <div class="d-flex justify-content-between">
+          <p>Your Lease Payment</p>
+          <p class="text-dark font">$230.35</p>
+      </div>
+      <div>
+          <button type="button" class="btn btn-primary">View Details</button>
+      </div>
+      <div class="text-center p-3">
+          <i class="fa-regular fa-trash-can fs-3 text-muted"></i>
+      </div>
+  </div>
+      `)
+    }
+  })
+}
--- FormProducts/script.js
+++ FormProducts/script.js
@@ -1,5 +1,6 @@
 let products = [];
-
+let index=0;
+let total;
 $('#form').submit((e) => {
     e.preventDefault();
     const product = {
@@ -23,6 +24,7 @@ $('#form').submit((e) => {
 })
 
 function insertInToTable(products){
+    let totalPrice=0;
     for(let i=0; i<products.length; i++){
         productName = products[i].productName;
         quantity = products[i].quantity;
@@ -31,13 +33,29 @@ function insertInToTable(products){
         <tr>
             <td>${i + 1}</td>
             <td>${productName}</td>
-            <td>${quantity}</td>
+            <td class="td2"><input type="text" value="${quantity}" style="width:50px; padding:5px;text-align-center" class="quantity"></td>
             <td>${productCost}</td>
+            <td class="total">${quantity * productCost}</td>
+            <td><i class="fa-solid fa-trash deleteProduct"></i></td>
         </tr>
         `);
+        totalPrice+=quantity * productCost;
+        $("table tfoot td").text(totalPrice);
     }
 }
 
+$("table").on("click",".deleteProduct",(e)=>{
+    e.preventDefault();
+    $(event.target).parent().parent().remove();
+    let index=$(event.target).parent().parent().index();
+    products.splice(index,1);
+    let total=$(".total").text();
+    $("table tfoot td").text(total);
+})
+
 function clearTableData(){
-    $('table tbody').empty();
+    $("table tbody").empty();
 }
+$(".quantity").on("change",function(){
+    console.log("change happends");
+});
